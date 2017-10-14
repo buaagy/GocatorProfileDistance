d4ef7fff2d3936a859d0bf79ee718bfd698aeacd
3DD3 <utyg2015@163.com>
2017-10-14 15:27:34 +0800

    Update generate.py

--- GdkProfileDistance/generate.py
+++ GdkProfileDistance/generate.py
@@ -12,8 +12,8 @@ def Generate():
     print('Generating Gdk Sample solution')
     import Platform.scripts.Utils.kGenerator as generator
 
-    generator.WriteSolution(os.path.join(workDir, 'GdkProfileDistance', 'Gdk.xml'), ['MsvcChooser'], ['Win32', 'Win64', 'Sensor'])
-    generator.WriteSolution(os.path.join(workDir, 'GdkProfileDistance', 'Gdk.xml'), ['Ccs6Mk', 'WrWbMk'], ['C64x', 'Arm7'])
+    generator.WriteSolution(os.path.join(workDir, 'GocatorProfileDistance', 'Gdk.xml'), ['MsvcChooser'], ['Win32', 'Win64', 'Sensor'])
+    generator.WriteSolution(os.path.join(workDir, 'GocatorProfileDistance', 'Gdk.xml'), ['Ccs6Mk', 'WrWbMk'], ['C64x', 'Arm7'])
 
 if __name__ == '__main__':
     Generate()
